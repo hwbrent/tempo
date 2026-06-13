c10b9b48e8e9b0a18a227f25b8c75ead4d9b8864
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-06-13 12:09:38 +0100

    Calculate total month work days from getCurrentMonthAsWorkDays result

--- src/App.tsx
+++ src/App.tsx
@@ -31,35 +31,8 @@ function getCurrentMonthAsWorkDays(currentDate: Date): { [key: number]: boolean 
   return monthDays;
 }
 
-function getWorkDaysInCurrentMonth(currentDate: Date): number {
-  // Figure out the current year and month
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth();
-
-  // Loop through all days of the current month
-  let day = 1;
-  let totalWorkDays = 0;
-  while (true) {
-    const thisDate = new Date(year, month, day);
-    const thisMonth = thisDate.getMonth();
-
-    // Break when we get to the next month
-    if (thisMonth !== month) {
-      break;
-    }
-
-    // If the date is not the weekend, increment the total work days
-    const dotw = thisDate.getDay();
-    const isWeekend = dotw === 0 || dotw === 6;
-    if (!isWeekend) {
-      totalWorkDays++;
-    }
-
-    // Go to the next day
-    day++;
-  }
-
-  return totalWorkDays;
+function getTotalWorkDaysInMonth(monthDays: { [key: number]: boolean }): number {
+  return Object.values(monthDays).filter(Boolean).length;
 }
 
 function getMonthName(currentDate: Date): string {
@@ -69,8 +42,10 @@ function getMonthName(currentDate: Date): string {
 function App() {
   const currentDate = new Date();
   const monthName = getMonthName(currentDate);
-  const totalWorkDays = getWorkDaysInCurrentMonth(currentDate);
   const dotm = currentDate.getDate();
+
+  const monthDays = getCurrentMonthAsWorkDays(currentDate);
+  const totalWorkDays = getTotalWorkDaysInMonth(monthDays);
   return (
     <div>
       <p>Total work days in {monthName}: {totalWorkDays}</p>
