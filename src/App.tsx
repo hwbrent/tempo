6b23f3fbbc16b19ae3af11dcc00d6d3aa072eca2
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-06-13 12:05:54 +0100

    Add function to figure out whether every day in the current month is a workday or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+function getCurrentMonthAsWorkDays(currentDate: Date): { [key: number]: boolean } {
+  const monthDays = {};
+
+  // Figure out the current year and month
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+
+  // Loop through all days of the current month
+  let day = 1;
+  while (true) {
+    const thisDate = new Date(year, month, day);
+    const thisMonth = thisDate.getMonth();
+
+    // Break when we get to the next month
+    if (thisMonth !== month) {
+      break;
+    }
+
+    // record whether the current day is a workday (i.e. not a weekend)
+    const dotw = thisDate.getDay();
+    const isWeekend = dotw === 0 || dotw === 6;
+    monthDays[day] = !isWeekend;
+
+    // Go to the next day
+    day++;
+  }
+
+  return monthDays;
+}
+
 function getWorkDaysInCurrentMonth(currentDate: Date): number {
   // Figure out the current year and month
   const year = currentDate.getFullYear();
