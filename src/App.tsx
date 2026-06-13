5de8b1e101d95d9dbbc52e3937ff209c70525d5f
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-06-13 12:22:45 +0100

    Clean up table a bit

--- src/App.tsx
+++ src/App.tsx
@@ -63,10 +63,22 @@ function App() {
   const table = (
     <table>
       <tbody>
-        <tr><td>Total work days in {monthName}</td>         <td>{totalWorkDays}</td></tr>
-        <tr><td>Current day of the month</td>               <td>{dotm}</td></tr>
-        <tr><td>Work days of the month so far</td>          <td>{workDaysUpToToday}</td></tr>
-        <tr><td>Percentage of the work month completed</td> <td>{completionPctgString}</td></tr>
+        <tr>
+          <td>Total work days in {monthName}</td>
+          <td>{totalWorkDays}</td>
+        </tr>
+        <tr>
+          <td>Current day of the month</td>
+          <td>{dotm}</td>
+        </tr>
+        <tr>
+          <td>Work days of the month so far</td>
+          <td>{workDaysUpToToday}</td>
+        </tr>
+        <tr>
+          <td>Percentage of the work month completed</td>
+          <td>{completionPctgString}</td>
+        </tr>
       </tbody>
     </table>
   );
