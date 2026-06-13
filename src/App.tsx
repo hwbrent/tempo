847f85b0aed70f6f9e8c34ccd684681a688e60d2
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-06-13 11:51:18 +0100

    Calculate how many work days in month

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return null;
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  // Loop through all days of the month
+  let day = 1;
+  let totalWorkDays = 0;
+  while (true) {
+    const thisDate = new Date(year, month, day);
+    const thisMonth = thisDate.getMonth();
+
+    // Break when we get to the next month
+    if (thisMonth !== month) {
+      break;
+    }
+
+    // If the date is not the weekend, increment the total work days
+    const dotw = thisDate.getDay();
+    const isWeekend = dotw === 0 || dotw === 6;
+    if (!isWeekend) {
+      totalWorkDays++;
+    }
+
+    // Go to the next day
+    day++;
+  }
+
+  return <p>Total work days in {month}: {totalWorkDays}</p>;
 }
 
 export default App
