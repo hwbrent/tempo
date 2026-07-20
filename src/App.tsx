8170437c9622eecb401ec9694d7551ef7e9398bd
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-07-20 22:05:21 +0100

    Make Card more dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -56,25 +56,17 @@ function getTotalWorkDaysUpToDay(monthDays: MonthDays, dotm: number): number {
 
 function Card(props: {dotm: number, monthName: string, year: number, totalWorkDays: number, workDaysUpToToday: number, completionPctgString: string}): JSX.Element {
   const { dotm, monthName, year, totalWorkDays, workDaysUpToToday, completionPctgString } = props;
+  const rows = [
+    ["Today's date", `${dotm} ${monthName} ${year}`],
+    [`Total work days in ${monthName}`, totalWorkDays],
+    ['Work days of the month so far', workDaysUpToToday],
+    ['Percentage of the work month completed', completionPctgString],
+  ]
+  const trs = rows.map(([left, right]) => <tr><td>{left}</td><td>{right}</td></tr>);
   return (
     <table id='card'>
       <tbody>
-        <tr>
-          <td>Today's date</td>
-          <td>{dotm} {monthName} {year}</td>
-        </tr>
-        <tr>
-          <td>Total work days in {monthName}</td>
-          <td>{totalWorkDays}</td>
-        </tr>
-        <tr>
-          <td>Work days of the month so far</td>
-          <td>{workDaysUpToToday}</td>
-        </tr>
-        <tr>
-          <td>Percentage of the work month completed</td>
-          <td>{completionPctgString}</td>
-        </tr>
+        {trs}
       </tbody>
     </table>
   );
