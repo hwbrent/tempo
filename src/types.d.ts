2809fa16e510324f4246a88a35f59b4dd74a8132
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-07-20 22:22:36 +0100

    Strike through past days (except weekends)

---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @summary An object representing a month, where the keys are the day of the month, and
+ * the values indicate whether that day is a workday
+ */
+export type MonthDays = { [key: number]: boolean };
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,4 @@
-/**
- * @summary An object representing a month, where the keys are the day of the month, and
- * the values indicate whether that day is a workday
- */
-export type MonthDays = { [key: number]: boolean };
+import type { MonthDays } from './types';
 
 export const roundPercentage = (num: number) => Number(num.toFixed(1));
 
